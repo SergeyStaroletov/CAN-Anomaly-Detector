ee0ba424fee85253ac51fcdc3877761c42532ce8
Sergey <serg_soft@mail.ru>
2020-07-02 14:53:09 +0700

    Added stub for Exponential Moving Average detector

--- anomalypredictorstatistic.cpp
+++ anomalypredictorstatistic.cpp
@@ -1,3 +1,38 @@
 #include "anomalypredictorstatistic.h"
+#include <QDateTime>
+#include <QDebug>
 
 AnomalyPredictorStatistic::~AnomalyPredictorStatistic() = default;
+
+AnomalyPredictorStatistic::AnomalyPredictorStatistic() {
+  numPointsToPredict = 11000;
+  curentPoints = 0;
+  decay = 0.5;
+  ema = 0;
+  mseError = 0;
+}
+
+void AnomalyPredictorStatistic::getNewDataToPredict(CarState carstate) {
+  curentPoints++;
+
+  if (curentPoints == 1) carstateOld = carstate;
+
+  // todo: include not only speed
+  ema = ema * decay + (1.0 - decay) * carstateOld.speed;
+
+  mseError += (ema - carstate.speed) * (ema - carstate.speed);
+
+  carstateOld = carstate;
+
+  if (curentPoints >= numPointsToPredict) {
+    if (mseError > maxAnomalyScore) {
+      qDebug() << "Statistical anomaly for last " << numPointsToPredict
+               << " points detected at "
+               << QDateTime::fromMSecsSinceEpoch(carstate.timestamp).toString();
+    }
+
+    ema = 0;
+    curentPoints = 0;
+    mseError = 0;
+  }
+}
--- anomalypredictorstatistic.h
+++ anomalypredictorstatistic.h
@@ -6,7 +6,22 @@ class AnomalyPredictorStatistic : public AnomalyPredictor {
  public:
   AnomalyPredictorStatistic();
   virtual void getNewDataToPredict(CarState carstate);
+  void setNumPoints(unsigned numPointsToPredict) {
+    this->numPointsToPredict = numPointsToPredict;
+  }
+  void setMaxAnomalyScore(double maxAnomalyScore) {
+    this->maxAnomalyScore = maxAnomalyScore;
+  }
   virtual ~AnomalyPredictorStatistic();
+
+ private:
+  unsigned numPointsToPredict;
+  unsigned curentPoints;
+  double ema;
+  double decay;
+  CarState carstateOld;
+  double mseError;
+  double maxAnomalyScore;
 };
 
 #endif  // ANOMALYPREDICTORSTATISTIC_H
